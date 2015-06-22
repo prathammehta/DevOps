d8e01e4ddc8aab859ea4a7f75abab6650b588efc
Pratham Mehta <prathammehta@outlook.com>
2015-06-22 17:32:04 +0530

    Download for TFS

--- ContosoWebsite.ps1
+++ ContosoWebsite.ps1
@@ -13,6 +13,7 @@ Configuration ContosoWebsite
 
     $TFSSource = "http://download.microsoft.com/download/3/1/1/31149D54-CE97-4403-99E2-EBBEB790B718/vs2013.4_tfs_enu.iso"
     $TFSDest = "c:\users\public\documents\tfs.iso"
+    Invoke-WebRequest $TFSSource -outFIle $TFSDest
     Mount-DiskImage -ImagePath C:\Temp\vs2013.3_tfs_enu.iso
     Invoke-Expression "F:\TFS_server.exe /quiet /install"
 
