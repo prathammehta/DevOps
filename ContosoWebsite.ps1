1b34dec7af41c3aa18533984a1f5f7b6b92b0361
Pratham Mehta <prathammehta@outlook.com>
2015-06-30 19:40:49 +0530

    Logging added

--- ContosoWebsite.ps1
+++ ContosoWebsite.ps1
@@ -2,6 +2,8 @@
     $dest = "C:\Program Files\WindowsPowerShell\Modules\resource.zip"
     Invoke-WebRequest $source -OutFile $dest 
     
+    "$(Get-Date) Module downloaded" | Out-File -FilePath C:\users\Public\Documents\logs.txt -Append
+
     $file = $dest 
     $destination = "C:\Program Files\WindowsPowerShell\Modules"
 
@@ -11,6 +13,8 @@
     {
         $shell.Namespace($destination).copyhere($item)
     }
+
+    "$(Get-Date) Module Unzipped" | Out-File -FilePath C:\users\Public\Documents\logs.txt -Append
     
     cd "C:\Program Files\WindowsPowerShell\Modules\DevOpsDSCResources-master"
     Move-Item -Path "C:\Program Files\WindowsPowerShell\Modules\DevOpsDSCResources-master\ContosoDscResources" -Destination "C:\Program Files\WindowsPowerShell\Modules" 
@@ -18,8 +22,12 @@
     Remove-Item $dest -Force -Recurse
     Remove-Item "C:\Program Files\WindowsPowerShell\Modules\DevOpsDSCResources-master" -Force -Recurse
 
+    "$(Get-Date) Module CleanUp completed" | Out-File -FilePath C:\users\Public\Documents\logs.txt -Append
+
     Add-WindowsFeature dsc-service
     Get-dscresource
+
+    "$(Get-Date) Get DSCResource Completed" | Out-File -FilePath C:\users\Public\Documents\logs.txt -Append
   
  $dscconfig = @'
  Configuration ContosoWebsite
@@ -28,7 +36,8 @@
 
     Import-DSCResource -ModuleName ContosoDscResources
     
-     
+    "$(Get-Date) Module Imported" | Out-File -FilePath C:\users\Public\Documents\logs.txt -Append 
+
     Node $MachineName
     {
 
@@ -38,13 +47,21 @@
         {
         }
 
+        "$(Get-Date) Print server installed" | Out-File -FilePath C:\users\Public\Documents\logs.txt -Append
+
         ContosoWebServer DisableWebServer
         {
         }
+
+        "$(Get-Date) WebServer Disabled" | Out-File -FilePath C:\users\Public\Documents\logs.txt -Append
     }
    }
 '@
 
 Invoke-Expression $dscconfig
 
+"$(Get-Date) Node block invoked" | Out-File -FilePath C:\users\Public\Documents\logs.txt -Append
+
 ContosoWebsite -MachineName devvm0 
+
+"$(Get-Date) Configuration function called" | Out-File -FilePath C:\users\Public\Documents\logs.txt -Append
