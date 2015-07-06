a68462e47e72f36002c675cc13097e007acc562a
Pratham Mehta <prathammehta@outlook.com>
2015-07-06 22:09:01 +0530

    New files adde

---
+++ ContosoWebsite/DNSConfig.ps1
@@ -0,0 +1,77 @@
+Configuration DNSConfig
+{ 
+     param
+    ( 
+         
+        [Parameter(Mandatory=$true)][string]$DomainName = "internaldomain.com",
+        [Parameter(Mandatory=$true)][string]$DomainAdminUsername,
+        [Parameter(Mandatory=$true)][string]$DomainAdminPassword
+    ) 
+        
+    #Import the required DSC Resources  
+    Import-DscResource -ModuleName xComputerManagement 
+    Import-DscResource -ModuleName DevOpsActiveDirectory
+    Import-DscResource -ModuleName xActiveDirectory
+    Import-DscResource -ModuleName xRemoteDesktopAdmin
+
+    $securePassword = ConvertTo-SecureString -AsPlainText $DomainAdminPassword -Force;
+    $DomainAdminCred = New-Object System.Management.Automation.PSCredential($DomainAdminUsername, $securePassword);
+   
+    Node 'localhost'
+    { 
+	#ConfigurationBlock
+    
+	RemoteDesktopAdmin RDPAdmin 
+	{
+		Ensure = "Present"
+		serAuthentication = "NonSecure"
+	}
+
+        WindowsFeature DSCService 
+	{
+            	Name = "DSC-Service"
+            	Ensure = "Present"
+            	IncludeAllSubFeature = $true
+	    	DependsOn = "[xRemoteDesktopAdmin]RDPAdmin"
+        }
+         
+        WindowsFeature ADDSInstall 
+        {   
+            	Ensure = 'Present'
+            	Name = 'AD-Domain-Services'
+            	IncludeAllSubFeature = $true
+        }
+         
+        WindowsFeature RSATTools 
+        { 
+            	DependsOn= '[WindowsFeature]ADDSInstall'
+            	Ensure = 'Present'
+            	Name = 'RSAT-AD-Tools'
+            	IncludeAllSubFeature = $true
+        }
+
+        ADDomain SetupDomain 
+	{	
+            	DomainName = $DomainName
+            	DomainAdministratorUsername = $DomainAdminUsername
+            	DomainAdministratorPassword = $DomainAdminPassword
+            	SafemodeAdministratorUsername = $DomainAdminUsername
+            	SafemodeAdministratorPassword = $DomainAdminPassword
+            	DependsOn='[WindowsFeature]RSATTools'
+        }
+ 
+        ADDomainController SetupDomainController 
+	{
+            	DomainName = $DomainName
+            	DomainAdministratorUsername = $DomainAdminUsername
+            	DomainAdministratorPassword = $DomainAdminPassword
+            	SafemodeAdministratorUsername = $DomainAdminUsername
+            	SafemodeAdministratorPassword = $DomainAdminPassword
+            	DependsOn='[ADDomain]SetupDomain'
+        }	
+
+    #End Configuration Block    
+    } 
+}
+
+# Restart-Computer -ComputerName "localhost" -Force
---
+++ ContosoWebsite/DevOpsActiveDirectory/DevOpsActiveDirectory.psd1
@@ -0,0 +1,31 @@
+@{
+# Version number of this module.
+ModuleVersion = '1.0'
+
+# ID used to uniquely identify this module
+GUID = '27CCCBA6-E8D0-4496-B00D-79D75ECC7D03'
+
+# Author of this module
+Author = 'Anindit Karmakar'
+
+# Company or vendor of this module
+CompanyName = 'Microsoft Corporation'
+
+# Copyright statement for this module
+Copyright = '(c) 2014 Microsoft Corporation. All rights reserved.'
+
+# Description of the functionality provided by this module
+Description = 'This module installs an Active Directory Domain Services Forest in the target machine.'
+
+# Minimum version of the Windows PowerShell engine required by this module
+PowerShellVersion = '4.0'
+
+# Minimum version of the common language runtime (CLR) required by this module
+CLRVersion = '4.0'
+
+# Functions to export from this module
+FunctionsToExport = '*'
+
+# Cmdlets to export from this module
+CmdletsToExport = '*'
+}
